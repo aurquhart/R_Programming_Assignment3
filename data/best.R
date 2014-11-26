e18e49df41bdfbbc1d32119c69e612da2899a67a
Angus Urquhart <aurquhart@pmsi-consulting.com>
2014-11-26 18:03:29 +0000

    part 1 finished

--- data/best.R
+++ data/best.R
@@ -7,6 +7,11 @@ best <- function(state,outcome ) {
                   "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
                   "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" )]
   
+  
+  #Check that state in function exists in data
+  DoesStateExist<-subset(bestdf$State, bestdf$State==state)
+  if(length(DoesStateExist) == 0) stop("invalid state")
+  
   #Fix outcome so it's the same as what the user inputs
   if(outcome == "heart attack") {outcomeanswer <- 
                                    "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
@@ -16,14 +21,27 @@ best <- function(state,outcome ) {
                                             "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
   } else (outcome == "invalid state")
   
+  #outcomeanswer
   
   singledf <- bestdf[ which(bestdf$State== state), ]
   #head(singledf)
   singlemetric <- singledf[,c("Hospital.Name",outcomeanswer)]
+  
+  #convert answer to be numeric
+  singlemetric[, 2:2] <- sapply(singlemetric[, 2:2], as.numeric)
+  
+  #head(singlemetric)
   answer <-singlemetric[which(singlemetric[outcomeanswer] == 
                                 min(singlemetric[[outcomeanswer]],na.rm = TRUE)), ]
   answer$Hospital.Name
 }
 
+#state <- "NN"
+#state <- "NY"
+#outcome <- "pneumonia"
 
 #best("SC", "heart attack")
+#best("NY", "pneumonia")
+#best("NN","pneumonia")
+
+
